0f000cb574bc3fa2fe5084d50852754f520e9cdc
Samybaxy <elance@samybaxy.net>
2018-02-09 05:56:57 +0100

    Added a more performant solution

--- src/frogRiverOne.js
+++ src/frogRiverOne.js
@@ -22,3 +22,24 @@ function solution(X, A) {
     
     return -1;
 }
+
+//A more performant solution
+//https://app.codility.com/demo/results/training4TZYUG-2YP/
+function solution(X, A) {
+    // write your code in JavaScript (Node.js 8.9.4)
+    let sequence = [0];
+    let position = -1;
+    let counter = 0;
+
+    if (X === 1 && A[0] === 1) return 0;
+    for (let i = 0; i < A.length; i++) {
+        if (A[i] <= X) {
+            if (!sequence[A[i]]) counter++;
+            sequence[A[i]] = A[i];
+
+            if (counter === X) return i;
+        }
+    }
+
+    return -1;
+}
